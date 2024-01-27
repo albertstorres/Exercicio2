ecbe1fd2c910adbd484f652bb22f8edb3b5f1940
Albert Sevy <albertstorres@gmail.com>
2024-01-26 23:30:13 -0300

    Iniciando o repositório do Exercício2

---
+++ index.ts
@@ -0,0 +1,32 @@
+const usuarios = [
+    {
+        nome: "Guido",
+        idade: 32,
+        status: true
+    },
+    {
+        nome: "Dani",
+        idade: 30,
+        status: true
+    },
+    {
+        nome: "João",
+        idade: 40,
+        status: false
+    },
+    {
+        nome: "Guilherme",
+        idade: 29,
+        status: true
+    },
+    {
+        nome: "Ana",
+        idade: 18,
+        status: false
+    },
+    {
+        nome: "José",
+        idade: 28,
+        status: false
+    }
+];
